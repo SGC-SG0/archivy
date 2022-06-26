d2f6513809ee0361b329ceabae0b345533a9fd61
Oneill <oneill@p2x-3yz.us>
2022-06-26 18:16:16 -0500

    updated site link

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
     ),
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/archivy/archivy",
+    url="https://github.com/SGC-SG0/archivy",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
